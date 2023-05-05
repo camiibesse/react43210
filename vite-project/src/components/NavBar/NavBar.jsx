d0cc26cee3142ca7855482217e2850c10eedd1bf
camiibesse <camilabesse@hotmail.com>
2023-05-05 10:41:22 -0300

    modificando el css

--- vite-project/src/components/NavBar/NavBar.jsx
+++ vite-project/src/components/NavBar/NavBar.jsx
@@ -4,14 +4,16 @@ import CartWidget from "../CartWidget/CartWidget";
 function NavBar (){
     return (
         <nav>
+            <div>
+                <img src={logo} alt="Logo" />
+            </div>
             <ul>
                 <button>Playstation 4</button>
                 <button>Playstation 5</button>
                 <button>Accesorios</button>
-                <img src={logo} alt="Logo" />
             </ul>
             <div>
-            <CartWidget />
+            <CartWidget/>
             </div>
         </nav>
     )
